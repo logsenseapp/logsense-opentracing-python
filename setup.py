81935203e3f6237c517b0bdef10b9f95cbcb04ab
Dominik Rosiek <drosiek@collective-sense.com>
2019-07-01 15:50:11 +0200

    Add License to setup.py

--- setup.py
+++ setup.py
@@ -12,6 +12,9 @@ with open(join(package_dir, 'logsense_opentracing', 'version.py')) as f:
 with open(join(package_dir, 'README.md')) as f:
     README = f.read()
 
+with open(join(package_dir, 'LICENSE.md')) as f:
+    LICENSE = f.read()
+
 setup(
     name='logsense-opentracing-tracker',
     version=__version__,
@@ -20,7 +23,7 @@ setup(
     author='Dominik Rosiek',
     author_email='drosiek@collective-sense.com',
     url='https://github.com/collectivesense/logsense-opentracing-python',
-    license=None,
+    license=LICENSE,
     packages=find_packages(exclude=('examples',)),
     install_requires=[
         'opentracing==2.0.0',
